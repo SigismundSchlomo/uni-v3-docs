b312f5f96d2d02e46d4d7e7306faf9d2d0d0e2bd
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-04-10 17:54:15 +0300

    Add deposit script

---
+++ src/deposit.ts
@@ -0,0 +1,21 @@
+import ethers from 'ethers';
+import sAMB from './sAMB.json';
+
+
+const provider = new ethers.providers.JsonRpcProvider('');
+const wallet = new ethers.Wallet('0x', provider);
+
+const contract = new ethers.Contract(
+    '',
+    sAMB.abi,
+    provider
+);
+
+
+async function main() {
+    const tx = await contract.deposit(ethers.utils.parseEther('1.0'));
+    console.log(tx);
+    const receipt = await tx.wait();
+    console.log(receipt);
+}
+
